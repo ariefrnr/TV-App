0f7cd46ebdaf638e1920fedeaa5c074635a34a78
ariefrnr <arief.roihan.nur@gmail.com>
2026-09-09 13:51:53 +0700

    Initial Commit

---
+++ TV App/TV_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TV_AppApp.swift
+//  TV App
+//
+//  Created by Arief Roihan Nur Rahman on 09/09/26.
+//
+
+import SwiftUI
+
+@main
+struct TV_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
